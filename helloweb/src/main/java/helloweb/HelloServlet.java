a50407d7ed4326a756fba008de2174d66382d1ed
born-A <93516595+born-A@users.noreply.github.com>
2024-05-21 15:57:50 +0900

    servlet-practices init

---
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -0,0 +1,27 @@
+package helloweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String id = request.getParameter("id"); // getParameter로 받는 것들은 String
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter pw = response.getWriter();
+		pw.print("<h1>안녕 " + id +"</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
